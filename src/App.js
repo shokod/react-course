d9a5170ec41e3521bd0fe72ea8b0950ef124096f
shokod <delvinshoko@gmail.com>
2023-12-13 11:49:00 +0200

    Add! using react link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Route, Routes } from  'react-router-dom';
+import { Route, Routes,} from  'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import NewMeetups from './pages/NewMeetups'
 import Favorites from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
       <Route path='/' element={<AllMeetups />}>
         
